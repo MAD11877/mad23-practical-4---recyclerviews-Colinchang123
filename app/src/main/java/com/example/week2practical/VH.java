6d1de115fc44c199d3cebcd59863ea4fd7f152c7
s10242330 <s10242330@connect.np.edu.sg>
2023-05-08 23:27:38 +0800

    1

--- app/src/main/java/com/example/week2practical/VH.java
+++ app/src/main/java/com/example/week2practical/VH.java
@@ -11,10 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 public class VH extends RecyclerView.ViewHolder {
     TextView textView;
     TextView descView;
+    ImageView imageView;
 
     public VH (View itemView){
         super (itemView);
         textView = itemView.findViewById(R.id.itemname);
         descView = itemView.findViewById(R.id.itemdesc);
+        imageView= itemView.findViewById(R.id.itemimage);
     }
 }
